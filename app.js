77274bcf9c895111386842fbd14f6076aaae3e37
djbetamax <hugots23@gmail.com>
2022-09-23 23:31:08 -0300

    feat: now the results appear under the button

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const input = document.querySelector('.numberValue')
-
-function multiplication (num) {
-  console.log('====================');
-    for (i = 1; i <= 10; i ++) {
-      num = input.value;
-      const result = num * i;
-
-      console.log(`${num} X ${i} =`, result);
-    }
-}
+const input = document.querySelector('.numberValue');
+const div = document.querySelector('.tableContainer');
+const tableDiv = document.querySelector('.tableResult');
 
 const btn = document.querySelector('.submitBtn')
-
-btn.addEventListener('click', function () {
-  multiplication();
+btn.addEventListener('click', function (num) {
+  if (input.value >= 1) {
+    for (i = 1; i <= 10; i++) {
+      num = input.value;
+      const result = num * i;
+      
+      // if (tableDiv.children >= 0) {
+        // ? tableDiv.replaceChildren()
+      // }
+      const p = document.createElement('p');
+        tableDiv.appendChild(p);
+      const tablePattern = document.createTextNode(`${num} X ${i} = ${result}`);
+        p.appendChild(tablePattern);
+        div.classList.remove('showContainer');
+    } 
+  } else {
+      alert('Need to be a number higher then 0!');
+    }
   input.value = 1;
 })
-
-// show the multiplication table
-const div = document.querySelector(('.tableResult'));
